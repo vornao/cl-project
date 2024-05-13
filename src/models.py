846e39ac46d83aa00c528863ffad3191cf412295
Luca <migliorluca@gmail.com>
2024-05-13 17:37:01 +0200

    no crash on federated but loss not working

--- src/models.py
+++ src/models.py
@@ -47,8 +47,9 @@ class MNISTConvNetTrainer():
         self.device = torch.device(device)
 
         self.model.to(self.device)
+        self.clients = {}
         print(f'Model created and moved to {device}')
-        
+
 
     def train(self, num_epochs):
         self.model.train()
@@ -105,50 +106,146 @@ class MNISTConvNetTrainer():
         print(f'Model saved to {path}')
 
 
-
 class MNISTFederatedClient():
-    def __init__(self, k: int, local_epochs:int, local_dataset: DataLoader, lr:float, model_state: Dict) -> None:
+    def __init__(self, k: int, local_epochs:int, lr:float, device='cpu') -> None:
         self.k = k
-        self.model = MNISTConvNet()
-        self.model.load_state_dict(model_state)
-
+        self.lr = lr
         self.epochs = local_epochs
-        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=lr)
-        self.local_dataset = local_dataset
+        self.device = torch.device(device)
+
+    def eval(self, dataset: DataLoader, model):
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for images, labels in dataset:
+                images,labels = images.to(self.device), labels.to(self.device)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted==labels).sum().item()
+            
+            
     
-    def __call__(self):
+    def __call__(self, dataset: DataLoader, model_state: Dict):
         # training with federated learning
-        self.model.train()
+        model = MNISTConvNet()
+        model.load_state_dict(model_state)
+        optimizer = torch.optim.SGD(model.parameters(), lr=self.lr)
+        criterion = nn.CrossEntropyLoss()
+        model.train()
         for epoch in range(self.epochs):
-            for images, labels in self.local_dataset:
+            for images, labels in dataset:
                 images, labels = images.to(self.device), labels.to(self.device)
-                self.optimizer.zero_grad()
-                outputs = self.model(images)
-
+                optimizer.zero_grad()
+                outputs = model(images)
                 # only updating local weights
-                loss = F.cross_entropy(outputs, labels)
+                loss = criterion(outputs, labels)
                 loss.backward()
-                self.optimizer.step()
-
+                optimizer.step()
         # return local model state
-        return self.model.state_dict()
+        # evaluate the model
+        self.eval(dataset, model)
+        return model.state_dict()
 
 
 class MNISTFederatedServer():
-    def __init__(self, k: int, lr: float, model_state: Dict) -> None:
-        self.k = k
-        self.model = MNISTConvNet()
-        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=lr)
+    def __init__(
+            self, t: int, 
+            lr: float, 
+            model: nn.Module, 
+            train_loader: DataLoader,
+            val_loader: DataLoader,
+            test_loader: DataLoader,
+            criterion: nn.Module = nn.CrossEntropyLoss(),
+            C=1,
+        ) -> None:
+    
+        self.train_loader = train_loader # remove this, each client should have its own data
+        self.val_loader = val_loader
+        self.test_loader = test_loader
+        self.k = 0
+        self.model = model
+        self.lr = lr
+        self.clients: Dict[int, MNISTFederatedClient] = {}
+        self.rounds = t
+        self.C = C
+        self.m = max([self.C*self.k, 1])
+        self.criterion = criterion
 
-    def aggregate(self, local_states: List[Dict]):
+
+    def init_clients(self, num_clients, local_epochs, lr):
+        self.k = num_clients
+        for i in range(num_clients):
+            self.clients[i] = MNISTFederatedClient(
+                k=1, 
+                local_epochs=local_epochs, 
+                lr=lr
+            )
+
+
+    def update_global_model(self, local_states: List[Dict]):
         # aggregate local model states
-        for key in self.model.state_dict().keys():
-            self.model.state_dict()[key] = sum([state[key] for state in local_states]) / len(local_states)
+        # ratio = [len_indexes[i] / sum(len_indexes) for i in range(self.k
+        new_states_dict = {}
+        for param in self.model.state_dict().keys():
+            new_states = sum([state[param] for state in local_states]) 
+            new_states_dict[param] = new_states / self.k
         
-    def start_train(self, clients: List[MNISTFederatedClient]):
-        # aggregate local model states
-        local_states = [client() for client in clients]
-        self.aggregate(local_states)
+        # update global model
+        self.model.load_state_dict(new_states_dict)
+
+
+    def _copy_state(self, state):
+        new_state = {}
+        for key, value in state.items():
+            new_state[key] = value.clone()
+        return new_state
+    
+    def start_train(self):
+
+        client_data = []
+
+        for _ in range(self.rounds):
+            local_states = []
+            len_indexes  = []
+            indexes = torch.randperm(len(self.train_loader.dataset))
+            for i in range(self.k):
+                start = i * self.m
+                end = min((i+1)*self.m, len(indexes))
+                len_indexes.append(end-start)
+                loader = DataLoader(self.train_loader.dataset, batch_size=self.m, sampler=indexes[start:end])
+                client_data.append(loader)
+        
+            for client, loader in zip(self.clients.values(), client_data):
+                # create a copy of self.model.state_dict() for each client
+                state_copy = self._copy_state(self.model.state_dict())
+                s = client(loader, self.model.state_dict())
+                local_states.append(s)
+            
+            self.update_global_model(local_states)
+
+            # evaluate the model
+            self.model.eval()
+            test_loss, test_acc = self.test(self.test_loader)
+            print(f'Round {_}, Test Loss: {test_loss}, Test Acc: {test_acc}')
+
+    def test(self, test_loader=None):
+        self.model.eval()
+        correct = 0
+        total = 0
+        loss = 0
+        with torch.no_grad():
+            for images, labels in self.test_loader:
+                outputs = self.model(images)
+                loss += self.criterion(outputs, labels)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        accuracy = correct / total
+        loss = loss / len(self.test_loader)
+        return loss, accuracy
+    
 
 
 # create MNIST dataloader
