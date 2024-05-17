17b17e1c6fd68a5fac7d6f294a0077e00c318d75
Luca <migliorluca@gmail.com>
2024-05-17 17:49:35 +0200

    add comments to code

--- src/models.py
+++ src/models.py
@@ -79,26 +79,14 @@ class ConvNetTrainer():
 
         progress_bar = tqdm(range(num_epochs), ncols=128)
         for epoch in progress_bar:
-            epoch_loss = 0
-            correct = 0
-            total = 0
-            for images, labels in self.train_loader:
-                images, labels = images.to(self.device), labels.to(
-                    self.device
-                )  # Move data to device (GPU if available)
-                self.optimizer.zero_grad()
-                outputs = self.model(images)
-                loss = self.criterion(outputs, labels)
-                loss.backward()
-                self.optimizer.step()
 
-                epoch_loss += loss.item()
-                _, predicted = torch.max(outputs.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
-
-            epoch_loss = epoch_loss / len(self.train_loader)
-            epoch_acc = correct / total
+            epoch_loss, _ = train_loop(
+                model=self.model,
+                optimizer=self.optimizer,
+                criterion=self.criterion,
+                sample_data=self.train_loader,
+                device=self.device
+            )
 
             # compute test round
             test_loss, test_acc = self.test(self.test_loader)
@@ -144,12 +132,46 @@ class ConvNetTrainer():
 
 
 class FederatedClient:
+    """
+    Represents a federated learning client.
+
+    Args:
+        k (int): The client identifier.
+        local_epochs (int): The number of local epochs to train the model.
+        lr (float): The learning rate for the optimizer.
+        device (str, optional): The device to use for training. Defaults to "mps".
+        dataset (torch.utils.data.Dataset, optional): The training dataset. Defaults to None.
+        n_samples (int, optional): The number of samples to use for training. Defaults to 1000.
+        weight_decay (float, optional): The weight decay for the optimizer. Defaults to 1e-6.
+        criterion (torch.nn.Module, optional): The loss criterion. Defaults to nn.CrossEntropyLoss().
+        model (torch.nn.Module, optional): The model architecture. Defaults to None.
+        sample_method (str, optional): The method for sampling the data. Defaults to "iid".
+
+    Attributes:
+        k (int): The client identifier.
+        lr (float): The learning rate for the optimizer.
+        epochs (int): The number of local epochs to train the model.
+        device (torch.device): The device to use for training.
+        model (torch.nn.Module): The model architecture.
+        optimizer (torch.optim.Optimizer): The optimizer for training.
+        criterion (torch.nn.Module): The loss criterion.
+        train_loader (torch.utils.data.DataLoader): The training data loader.
+        n_samples (int): The number of samples to use for training.
+        sample_method (str): The method for sampling the data.
+
+    Methods:
+        eval(dataset): Evaluates the model on a given dataset.
+        _sample_data_iid(): Samples data in an i.i.d. manner.
+        _sample_data_noniid(): Samples data in a non-i.i.d. manner.
+        __call__(state_dict): Trains the model using federated learning.
+
+    """
 
     def __init__(
             self, k: int, 
             local_epochs: int, 
             lr: float, 
-            device="cpu", 
+            device="mps", 
             dataset=None, 
             n_samples=1000, 
             weight_decay=1e-6, 
@@ -157,7 +179,21 @@ class FederatedClient:
             model=None,
             sample_method="iid"
         ) -> None:
+        """
+        Initializes a FederatedClient object.
 
+        Args:
+            k (int): The client identifier.
+            local_epochs (int): The number of local epochs to train the model.
+            lr (float): The learning rate for the optimizer.
+            device (str, optional): The device to use for training. Defaults to "mps".
+            dataset (torch.utils.data.Dataset, optional): The training dataset. Defaults to None.
+            n_samples (int, optional): The number of samples to use for training. Defaults to 1000.
+            weight_decay (float, optional): The weight decay for the optimizer. Defaults to 1e-6.
+            criterion (torch.nn.Module, optional): The loss criterion. Defaults to nn.CrossEntropyLoss().
+            model (torch.nn.Module, optional): The model architecture. Defaults to None.
+            sample_method (str, optional): The method for sampling the data. Defaults to "iid".
+        """
         self.k = k
         self.lr = lr
         self.epochs = local_epochs
@@ -171,10 +207,16 @@ class FederatedClient:
         self.train_loader = dataset
         self.n_samples = n_samples  
         self.sample_method = sample_method
-        # printu summary of all parameters
+        # print summary of all parameters
         print(f"> Client {k} created, lr: {lr}, epochs: {local_epochs}, samples: {n_samples}, device: {device}, weight_decay: {weight_decay}")
 
     def eval(self, dataset: DataLoader):
+        """
+        Evaluates the model on a given dataset.
+
+        Args:
+            dataset (torch.utils.data.DataLoader): The dataset to evaluate the model on.
+        """
         correct = 0
         total = 0
         loss = 0
@@ -192,13 +234,25 @@ class FederatedClient:
         print(f"Local Acc: {correct/total}, Local Loss: {loss/len(dataset)}")
 
     def _sample_data_iid(self):
+        """
+        Samples data in an i.i.d. manner.
+
+        Returns:
+            torch.utils.data.DataLoader: The data loader for the sampled data.
+        """
         subset = Subset(
             self.train_loader.dataset, 
             list(RandomSampler(self.train_loader.dataset, num_samples=self.n_samples)))
         return DataLoader(subset, batch_size=32, shuffle=True)
     
     def _sample_data_noniid(self):
-        # select a a key from the mnist dataset randomly
+        """
+        Samples data in a non-i.i.d. manner.
+
+        Returns:
+            torch.utils.data.DataLoader: The data loader for the sampled data.
+        """
+        # select a key from the mnist dataset randomly
         subset_a = [s for s in self.train_loader.dataset if s[1] == self.k % 10]
         subset_b = [s for s in self.train_loader.dataset if s[1] == (self.k + 1) % 10]
         # sample n_samples from the subset
@@ -209,47 +263,34 @@ class FederatedClient:
     
         return DataLoader(subset, batch_size=32, shuffle=True)
     
-    # def __sample_cifar10(self):
-    #     subset = Subset(
-    #         self.train_loader.dataset,
-    #         list(RandomSampler(self.train_loader.dataset, num_samples=self.n_samples))
-    #     )
-    #     # perform some data augmentation on cifar10 subset
-    #     return DataLoader(subset, batch_size=32, shuffle=True)
-    
-
     def __call__(self, state_dict ):
+        """
+        Trains the model using federated learning.
 
+        Args:
+            state_dict (dict): The state dictionary of the model.
+
+        Returns:
+            dict: The state dictionary of the trained model and the training history.
+        """
         sample_data = self._sample_data_iid() if self.sample_method == "iid" else self._sample_data_noniid()
 
         self.model.load_state_dict(state_dict)
-        # model to device
         self.model.to(self.device)
         history = {
             "loss": [],
             "acc": []
         }
         for _ in range(self.epochs):
-            self.model.train()
-            epoch_loss = 0
-            correct = 0
-            total = 0
 
-            for images, labels in sample_data:
-                images, labels = images.to(self.device), labels.to(self.device)
-                self.optimizer.zero_grad()
-                outputs = self.model(images)
-                loss = self.criterion(outputs, labels)
-                loss.backward()
-                self.optimizer.step()
+            epoch_loss, epoch_acc = train_loop(
+                model=self.model,
+                criterion=self.criterion,
+                optimizer=self.optimizer,
+                sample_data=sample_data, 
+                device=self.device
+            )
 
-                epoch_loss += loss.item()
-                _, predicted = torch.max(outputs.data, 1)
-                correct += (predicted == labels).sum().item()
-                total += labels.size(0)
-
-            epoch_loss = epoch_loss / len(sample_data)
-            epoch_acc = correct / total
             history["loss"].append(epoch_loss)
             history["acc"].append(epoch_acc)
             #print(f"Local Epoch {epoch+1}, Loss: {epoch_loss}, Accuracy: {epoch_acc}, client: {self.k}, total: {total}")
@@ -260,23 +301,28 @@ class FederatedClient:
         }
 
 
+
+
 class FederatedServer:
     def __init__(
         self,
         model: nn.Module,
         test_loader: DataLoader,
+        device: str = "mps",
     ) -> None:
 
         self.test_loader = test_loader
         self.global_model = model
+        self.device = torch.device(device)
         self.criterion = nn.CrossEntropyLoss()
+        self.global_model.to(self.device)
                 
 
-    def update_global_model(self, local_states: List[Dict]):
-        self.global_model.load_state_dict(self.aggregate(local_states))
+    def _update_global_model(self, local_states: List[Dict]):
+        self.global_model.load_state_dict(self._aggregate(local_states))
     
 
-    def aggregate(self, local_states: List[Dict]):
+    def _aggregate(self, local_states: List[Dict]):
         #print(f"Aggregating {len(local_states)} local states\n")
         new_state = {}
 
@@ -289,9 +335,10 @@ class FederatedServer:
             for key in state.keys():
                 new_state[key] += state[key]
 
+
         for key in new_state.keys():
-            l =  len(local_states)
             new_state[key] = new_state[key] / len(local_states) # assuming all clients have the same number of samples
+   
 
         return new_state
 
@@ -299,8 +346,8 @@ class FederatedServer:
     def start_train(self, clients, n_rounds, n_jobs=1):
         model_state = self.global_model.state_dict()   
         losses, accs = [], []
-
         pbar = tqdm(range(n_rounds), ncols=80)
+
         for _ in pbar:
             local_states = Parallel(n_jobs=n_jobs)(
                 delayed(c)(model_state) for c in clients
@@ -309,7 +356,7 @@ class FederatedServer:
             local_states = [state["state_dict"] for state in local_states]
             # create list of accuracies for eache model (a list of lists)
 
-            self.update_global_model(local_states)
+            self._update_global_model(local_states)
 
             test_loss, test_acc = self.test()
             losses.append(test_loss)
@@ -325,6 +372,7 @@ class FederatedServer:
         loss = 0
         with torch.no_grad():
             for images, labels in self.test_loader:
+                images, labels = images.to(self.device), labels.to(self.device)
                 outputs = self.global_model(images)
                 loss += self.criterion(outputs, labels)
                 _, predicted = torch.max(outputs.data, 1)
@@ -338,7 +386,24 @@ class FederatedServer:
 
 
 def init_clients(num_clients, local_epochs, lr, train_dataset, n_samples=1000, device="cpu", weight_decay=1e-6, criterion=nn.CrossEntropyLoss(), model=ConvNet(), sample_method="iid"):
+    """
+    Initializes a list of federated clients.
 
+    Args:
+        num_clients (int): The number of clients to initialize.
+        local_epochs (int): The number of local epochs for each client.
+        lr (float): The learning rate for each client.
+        train_dataset (torch.utils.data.Dataset): The training dataset.
+        n_samples (int, optional): The number of samples to use for each client. Defaults to 1000.
+        device (str, optional): The device to use for training. Defaults to "cpu".
+        weight_decay (float, optional): The weight decay for each client. Defaults to 1e-6.
+        criterion (torch.nn.Module, optional): The loss criterion for each client. Defaults to nn.CrossEntropyLoss().
+        model (torch.nn.Module, optional): The model architecture for each client. Defaults to ConvNet().
+        sample_method (str, optional): The method for sampling data for each client. Defaults to "iid".
+
+    Returns:
+        list: A list of initialized federated clients.
+    """
     clients = []
     for i in range(num_clients):
         clients.append(
@@ -363,6 +428,43 @@ def early_stopping(val_loss, patience=5):
         if all(val_loss[-1] > val_loss[-(i + 1)] for i in range(patience)):
             return True
     return False
+
+
+def train_loop(model, optimizer, criterion, sample_data, device):
+    """
+    Trains the given model using the provided optimizer and criterion on the given sample data.
+
+    Args:
+        model (torch.nn.Module): The model to be trained.
+        optimizer (torch.optim.Optimizer): The optimizer used for updating the model's parameters.
+        criterion (torch.nn.Module): The loss function used to compute the training loss.
+        sample_data (torch.utils.data.DataLoader): The data loader containing the training samples.
+        device (torch.device): The device on which the model and data should be loaded.
+
+    Returns:
+        tuple: A tuple containing the average training loss and the training accuracy for the epoch.
+    """
+    epoch_loss = 0
+    correct = 0
+    total = 0
+
+    model.train()
+    for images, labels in sample_data:
+        images, labels = images.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        _, predicted = torch.max(outputs.data, 1)
+        correct += (predicted == labels).sum().item()
+        total += labels.size(0)
+
+    epoch_loss = epoch_loss / len(sample_data)
+    epoch_acc = correct / total
+    return epoch_loss, epoch_acc
 
 
 def save_model(model, path):
