091d8ae23109a211f1fe3bdb83dc62164a50f04b
Luca <migliorluca@gmail.com>
2024-05-24 15:15:04 +0200

    minor fix

--- src/utils.py
+++ src/utils.py
@@ -24,6 +24,25 @@ def plot_mnist(train, width=10, height=5, cmap="gray"):
     fig.tight_layout()
 
 
+def plot_cifar10(train, width=10, height=5):
+    imgs = []
+    for i in range(10):
+        for img, label in train:
+            if label[0] == i:
+                imgs.append(img[0])
+                break
+
+    # plot the images
+    fig, axs = plt.subplots(2, 5, figsize=(width, height))
+    for i in range(10):
+        ax = axs[i // 5, i % 5]
+        ax.imshow(imgs[i].permute(1, 2, 0))
+        ax.set_title(f"Class {i}")
+        ax.axis("off")
+
+    fig.suptitle("CIFAR-10 dataset")
+    fig.tight_layout()
+
 # load config from../config.json
 def load_config():
     import json
